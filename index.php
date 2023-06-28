960aabeb9f88c973fe35d57ffe3f3b0a3a5e14c4
TheAlem <josealem03@gmail.com>
2023-06-27 23:33:52 -0400

    s

--- index.php
+++ index.php
@@ -1,9 +1,12 @@
 <?php
 require_once "libs/app.php";
 require_once "libs/controller.php";
-require_once "libs/model.php";
+require_once "models/model.php";
 require_once "libs/view.php";
 require_once 'libs/database.php';
+require_once "libs/registromodel.php";
+require_once "libs/db_config.php";
+require_once "libs/crud_clientes.php";
 $app = new App();
 
 //NAVEGACION AMIGABLE
