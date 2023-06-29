6b044b0de694d5b14d2cec1e69d48ac7d8944956
Jose Alem <josealem03@gmail.com>
2023-06-29 02:56:21 -0400

    asd

--- html/registromascota.php
+++ html/registromascota.php
@@ -333,16 +333,16 @@
                                         </tbody>
                                     </table>
                                 </div>
-                            </div>
-                            <!-- Footer -->
-                        <div class="row">
-                            <div class="col-md-12">
-                                <div class="copyright">
-                                    <p>Copyright © 2023 Colorlib. All rights reserved. Template byGrupo con 4 cambas.</p>
+                                <!-- Footer -->
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="copyright">
+                                            <p>Copyright © 2023 Colorlib. All rights reserved. Template byGrupo con 4 cambas.</p>
+                                        </div>
+                                    </div>
                                 </div>
+                                <!-- End of Footer -->
                             </div>
-                        </div>
-                        <!-- End of Footer -->
                         </div>
                     </div>
                 </div>
